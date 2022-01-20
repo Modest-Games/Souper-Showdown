5f19f7fa689fb67b2ad87ecc2099410576becf97
Mark McGregor <markmcgregor.contact@gmail.com>
2022-01-20 14:04:31 -0500

    Create soup pot behaviour

---
+++ Assets/Interactables/Soup Pot/SoupPot_Behaviour.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoupPot_Behaviour : MonoBehaviour
+{
+    public delegate void SoupPotDelegate();
+    public static event SoupPotDelegate SoupReceivedTrash;
+    public static event SoupPotDelegate SoupReceivedPlayer;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        switch (collision.gameObject.tag)
+        {
+            case "Pollutant":
+                // destroy the trash
+                Destroy(collision.gameObject);
+
+                // call the received trash event
+                if (SoupReceivedTrash != null)
+                    SoupReceivedTrash();
+
+                Debug.Log("Soup received pollutant!");
+
+                break;
+
+            case "Character":
+                // kill the player
+
+                // call the received player event
+                if (SoupReceivedPlayer != null)
+                    SoupReceivedPlayer();
+
+                Debug.Log("Soup received player!");
+
+                break;
+        }
+    }
+}
