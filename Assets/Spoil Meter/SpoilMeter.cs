880077bac30d321dc76dfde6728d184cfc41a141
Dylarno <medeirosdylan@hotmail.com>
2022-02-01 02:34:35 -0500

    Spoil Meter Implementation + Networking

---
+++ Assets/Spoil Meter/SpoilMeter.cs
@@ -0,0 +1,82 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Netcode;
+
+public class SpoilMeter : NetworkBehaviour
+{
+    [SerializeField] private float value;
+
+    private RectTransform maskTransform;
+    private float smoothTime;
+    private float velocity;
+    private float acceptableDifference;
+
+    void Start()
+    {
+        // 0-100 value of the Spoil Meter:
+        value = 0;
+
+        // The transform for the Spoil Meter's mask:
+        maskTransform = GetComponent<RectTransform>();
+
+        // Variables for smoothing value changes to the Spoil Meter:
+        smoothTime = 1.0f;
+        velocity = 0.0f;
+        acceptableDifference = 0.5f;
+    }
+
+    private void ChangeSpoilMeterValue(float pollutantValue)
+    {
+        // Should be turned in to a network variable:
+        value += pollutantValue;
+
+        // Change Spoil Meter for all clients:
+        ChangeSpoilMeterClientRpc(pollutantValue);
+    }
+
+    private IEnumerator SpoilMeterSmoothing(float target)
+    {
+        // Check if the position of the mask is "close enough" to where it should be:
+        while (Mathf.Abs(target - maskTransform.anchoredPosition.x) > acceptableDifference)
+        {
+            float newPosition = Mathf.SmoothDamp(maskTransform.anchoredPosition.x, target, ref velocity, smoothTime);
+            maskTransform.anchoredPosition = new Vector2(newPosition, 0f);
+
+            yield return new WaitForEndOfFrame();
+        }
+
+        // After the smoothing is done, set the mask's position to the actual target for consistency:
+        maskTransform.anchoredPosition = new Vector2(target, 0f);
+    }
+
+    private float CalculateMaskPosition(float current, float difference)
+    {
+        return (current + (difference * 18));
+    }
+
+    [ClientRpc]
+    private void ChangeSpoilMeterClientRpc(float pollutantValue)
+    {
+        // Convert the value change in relation to the Spoil Meter mask's x position:
+        var maskTarget = CalculateMaskPosition(maskTransform.anchoredPosition.x, pollutantValue);
+        StartCoroutine(SpoilMeterSmoothing(maskTarget));
+    }
+
+    private void OnSoupReceivedTrash()
+    {
+        ChangeSpoilMeterValue(10f);
+    }
+
+    private void OnEnable()
+    {
+        // setup event listeners
+        SoupPot_Behaviour.SoupReceivedTrash += OnSoupReceivedTrash;
+    }
+
+    private void OnDisable()
+    {
+        // clear event listeners
+        SoupPot_Behaviour.SoupReceivedTrash -= OnSoupReceivedTrash;
+    }
+}
