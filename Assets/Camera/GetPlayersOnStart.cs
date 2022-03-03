6f18e50b641e1616f2915e47a3ddb3f636a77424
TomasBabkineDiCaprio <babkinedicapriotomas@gmail.com>
2022-03-03 18:36:44 -0500

    Revert "Merge branch 'map-gen-merge' into dev"
    This reverts commit 9da0b9f00b7700059f1cedbd4e6ef9534b4d30c0, reversing
    changes made to 405f461dbafa44fec73fa351f2bd12b73de93085.

--- Assets/Camera/GetPlayersOnStart.cs
+++ Assets/Camera/GetPlayersOnStart.cs
@@ -7,8 +7,6 @@ using Cinemachine;
 public class GetPlayersOnStart : MonoBehaviour
 {
     private GameObject[] players;
-    private CinemachineVirtualCamera VCam;
-    private GameObject camObj;
 
     private void OnEnable()
     {
@@ -18,7 +16,7 @@ public class GetPlayersOnStart : MonoBehaviour
     private void OnGameStarted()
     {   
         // get camera target struct
-        CinemachineTargetGroup camTargetGroup = GameObject.Find("CineMachine Target Group").GetComponent<CinemachineTargetGroup>();
+        CinemachineTargetGroup camTargetGroup = GameObject.Find("CamTargetGroup").GetComponent<CinemachineTargetGroup>();
 
         // when the game starts, get all players 
         players = GameObject.FindGameObjectsWithTag("Player");
@@ -27,13 +25,6 @@ public class GetPlayersOnStart : MonoBehaviour
         foreach(GameObject player in players) {
             camTargetGroup.AddMember(player.transform, 1f, 0f);
         }
-
-        // update camera settings to center on characters
-        camObj = GameObject.Find("CineMachine VCam");
-        VCam = camObj.GetComponent<CinemachineVirtualCamera>();
-        VCam.GetCinemachineComponent<CinemachineFramingTransposer>().m_CameraDistance = 900;
-        VCam.GetCinemachineComponent<CinemachineFramingTransposer>().m_ScreenX = 0.5f;
-        VCam.GetCinemachineComponent<CinemachineFramingTransposer>().m_ScreenY = 0.5f;
 
     }
 
