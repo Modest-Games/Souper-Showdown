5b8cc9b7a9db10765e2e5765841ba4142328f21d
quinn rodriques <60561977+quintonrodriques@users.noreply.github.com>
2022-03-31 14:59:09 -0400

    Add Automated Graph
    Dynamically generated bar graph sorts and animates elements within graph panel

---
+++ Assets/EndScreenManager.cs
@@ -0,0 +1,182 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndScreenManager : MonoBehaviour
+{
+    //Particle materials
+   //public Material[] particles;
+
+    //Variables used for setting bar graph parameters
+    public float maxHeight = 523.0f;
+
+    public GameObject BarGraphPanel;
+
+    public int[] scores;
+    public GameObject PlayerBarPrefab;
+    public GameObject[] PlayerBars;
+
+    public int numPlayers;
+
+    public Material[] spoilerMaterials;
+    public Sprite[] spoilerIcons;
+    public Color[] spoilerColors;
+
+    public int[] presetScores = new int[]{ 1200, 3600, 4300, 2000};
+    public string[] spoilers = new string[] { "Tomato", "Carrot", "Mushroom", "Eggplant" };
+
+    Dictionary<string, int> spoilerDictionary;
+
+
+    void positionBars()
+    {
+        SortBars();
+    }
+
+    int getScoreFromBar(GameObject p)
+    {
+        return p.transform.GetChild(0).GetComponent<PlayerBar>().score;
+    }
+    
+
+    void setHeights()
+    {
+        float maxScore = getScoreFromBar(PlayerBars[0]);
+        float minScore = getScoreFromBar(PlayerBars[PlayerBars.Length - 1]);
+
+        for(int i = 0; i < PlayerBars.Length - 0; i++)
+        {
+            float val = Mathf.InverseLerp(minScore, maxScore, getScoreFromBar(PlayerBars[i]));
+            val = (val / 2.0f) + 0.45f;
+            setBarHeight(PlayerBars[i], val);
+        }
+
+    }
+
+    void setBarHeight(GameObject bar, float h)
+    {
+        bar.transform.GetChild(0).GetComponent<PlayerBar>().setHeight(h);
+    }
+
+    void SortBars()
+    {
+        int len = PlayerBars.Length;
+
+        for (int i = 0; i < len; i++)
+        {
+            for (int j = 0; j < len - i - 1; j++)
+            {
+                if (PlayerBars[j].transform.GetChild(0).GetComponent<PlayerBar>().score < PlayerBars[j + 1].transform.GetChild(0).GetComponent<PlayerBar>().score)
+                {
+                    //Swap player bars
+                    GameObject tmp = PlayerBars[j];
+                    PlayerBars[j] = PlayerBars[j + 1];
+                    PlayerBars[j + 1] = tmp;
+                }
+            }
+        }
+
+        foreach (GameObject pb in PlayerBars)
+        {
+            Debug.Log(pb.transform.GetChild(0).GetComponent<PlayerBar>().score);
+            pb.transform.SetParent(GameObject.FindGameObjectWithTag("BarGraph").transform, false);
+        }
+    }
+
+    void loadSpoilerLibrary()
+    {
+        //Initialize dictionary
+        spoilerDictionary = new Dictionary<string, int>();
+        spoilerDictionary.Add("Tomato", 0);
+        spoilerDictionary.Add("Carrot", 1);
+        spoilerDictionary.Add("Mushroom", 2);
+        spoilerDictionary.Add("Eggplant", 3);
+    }
+
+    GameObject MakeBar(string spoiler, int score) 
+    {
+        //Create player bar
+        GameObject playerBar = Instantiate(PlayerBarPrefab, new Vector3(0, 0, 0), Quaternion.identity);
+
+        int spoilerIndex;
+        bool hasValue = spoilerDictionary.TryGetValue(spoiler, out spoilerIndex);
+
+        if (!hasValue)
+            Debug.Log("spoiler not found!");
+        else
+        {
+            playerBar.transform.GetChild(0).GetComponent<PlayerBar>().setUpBar(score, 1.0f, spoilerMaterials[spoilerIndex], spoilerIcons[spoilerIndex], spoilerColors[spoilerIndex]);
+        }
+
+        //Place bar in panel
+        //playerBar.transform.SetParent(GameObject.FindGameObjectWithTag("BarGraph").transform, false);
+
+        return playerBar;
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        loadSpoilerLibrary();
+
+        PlayerBars = new GameObject[numPlayers];
+
+
+        //Create bars
+
+        /*
+        for(int i = 0; i < numPlayers; i++)
+        {
+            int tempScore = presetScores[i];
+            if (i % 2 == 0)
+            {
+                PlayerBars[i] = MakeBar("Carrot", tempScore);
+            }
+            else
+            {
+                PlayerBars[i] = MakeBar("Tomato", tempScore);
+            }
+        }
+        */
+
+        for (int i = 0; i < numPlayers; i++)
+        {
+            PlayerBars[i] = MakeBar(spoilers[i], presetScores[i]);
+        }
+
+
+        //PlayerBars[0] = MakeBar("Tomato", presetScores[0]);
+        //PlayerBars[1] = MakeBar("Carrot", presetScores[1]);
+        //PlayerBars[2] = MakeBar("Mushroom", presetScores[2]);
+        //PlayerBars[3] = MakeBar("Eggplant", presetScores[3]);
+
+        //Sort bars
+        SortBars();
+
+        //Give Verticality to bars
+        setHeights();
+
+        //StartAnimating
+        StartCoroutine(StaggerAnimation());
+    }
+
+
+    private IEnumerator StaggerAnimation()
+    {
+        for (int i = PlayerBars.Length - 1; i > 0; i--)
+        {
+            PlayerBars[i].transform.GetChild(0).GetComponent<PlayerBar>().startAnimating();
+            yield return new WaitForSeconds(0.5f);
+        }
+
+        yield return new WaitForSeconds(1.0f);
+        PlayerBars[0].transform.GetChild(0).GetComponent<PlayerBar>().startAnimating();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
